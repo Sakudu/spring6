9113ac5f701556bbda1c56378a9c92b838a27efb
saku <antinomy_bruno@163.com>
2023-10-10 10:04:34 +0800

    009

---
+++ spring6-006-beanLifeCycle/src/main/java/com/sysy/lifeCycle/Order.java
@@ -0,0 +1,8 @@
+package com.sysy.lifeCycle;
+
+/**
+ * @author saku
+ * @date 2023/10/10 9:49
+ */
+public class Order {
+}
--- spring6-006-beanLifeCycle/src/test/java/LifeCycleTest.java
+++ spring6-006-beanLifeCycle/src/test/java/LifeCycleTest.java
@@ -1,6 +1,8 @@
+import com.sysy.lifeCycle.Order;
 import com.sysy.lifeCycle.Person;
 import com.sysy.lifeCycle.User;
 import org.junit.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -25,5 +27,15 @@ public class LifeCycleTest {
         System.out.println("8 使用bean " + person);
         //必须手动关闭Spring容器 Spring容器才会销毁bean
         ac.close();
+    }
+
+    @Test
+    public void test3(){
+        Order order = new Order();
+        System.out.println(order);
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        factory.registerSingleton("order", order);
+        Order orderBean = factory.getBean("order", Order.class);
+        System.out.println(orderBean);
     }
 }
