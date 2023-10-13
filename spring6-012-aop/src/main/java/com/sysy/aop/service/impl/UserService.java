a7c4ddcc7a9ef3c95547d872b5237fdd97301443
saku <antinomy_bruno@163.com>
2023-10-13 11:55:01 +0800

    aop

---
+++ spring6-012-aop/src/main/java/com/sysy/aop/service/impl/UserService.java
@@ -0,0 +1,31 @@
+package com.sysy.aop.service.impl;
+
+/**
+ * @author saku
+ * @date 2023/10/12 16:23
+ */
+public class UserService {
+
+    public void do1(){
+        System.out.println("do 1");
+    }
+    public void do2(){
+        System.out.println("do 2");
+    }
+    public void do3(){
+        System.out.println("do 3");
+    }
+    public void do4(){
+        System.out.println("do 4");
+    }
+    public void do5(){
+        System.out.println("do 5");
+    }
+    // 核心业务方法
+    public void service(){
+        do1();
+        do2();
+        do3();
+        do5();
+    }
+}
